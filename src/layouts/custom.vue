900cdfc23cc0327b723036b437939993c99513b2
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-11-20 18:05:35 +0900

    created

---
+++ src/layouts/custom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <header>
+      <slot name="header">
+        Default header content
+      </slot>
+    </header>
+    <main>
+      <slot />
+    </main>
+    <footer>
+        <slot name="footer">
+            Default footer content
+        </slot>
+    </footer>
+  </div>
+</template>
