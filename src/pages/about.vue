16e80fa7c790b4ff32e970699235edd4265e985b
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-17 11:15:51 +0900

    useStateをグローバル関数として利用

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,17 @@
 <template>
     <div>
-        This is about page
+        <div>
+            This is about page
+        </div>
+        <p>ユーザーID: {{ user.id }}</p>
+        <p>ユーザー名: {{ user.name }}</p>
+        <p>メールアドレス: {{ user.mail }}</p>
     </div>
 </template>
+
+<script lang="ts" setup>
+    definePageMeta({
+        layout: false,
+    });
+    const user = useLoginUser();
+</script>
