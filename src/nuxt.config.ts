003064a24b5fbe8013d2cfd5f4fb2e3d69b753b0
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-22 12:21:27 +0900

    headとruntimeConfigを設定

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            title: 'Nuxt Dojo',
+            meta: [
+                { name: 'description', content: 'Everything about Nuxt 3'}
+            ],
+            link: [
+                {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'}
+            ]
+        }
+    },
     pages: true,
     css: ['~/assets/css/main.css'],
     postcss: {
@@ -9,6 +20,9 @@ export default defineNuxtConfig({
         },
     },
     runtimeConfig: {
-        productName: process.env.PRODUCT_NAME,
+        public: {
+            productName: process.env.PRODUCT_NAME,
+            APIBaseURL: process.env.API_BASE_URL,
+        }
     },
 })
