617bd3b7dd031eb85201547dc4d196d3602734b3
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-22 13:23:10 +0900

    server/api　作成

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -6,12 +6,23 @@
         <p>ユーザーID: {{ user.id }}</p>
         <p>ユーザー名: {{ user.name }}</p>
         <p>メールアドレス: {{ user.mail }}</p>
+
+        <div>{{ data }}</div>
     </div>
 </template>
 
 <script lang="ts" setup>
     definePageMeta({
-        layout: false,
+        layout: 'daisyui',
     });
     const user = useLoginUser();
+
+
+    // fetch server api
+    const { data } = await useFetch('/api/ninja?name=mario')
+    // const { data } = await useFetch('/api/ninja?name=mario', {
+    //     method: 'post',
+    //     body: {age: 30}
+    // })
+
 </script>
---
+++ src/server/api/products/[id].js
@@ -0,0 +1,9 @@
+export default defineEventHandler(async (event) => {
+    const { id } = event.context.params
+
+    const uri = `https://fakestoreapi.com/products/${id}`
+    
+    const { data } = await $fetch(uri)
+  
+    return data
+})
