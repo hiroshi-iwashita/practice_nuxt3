53764c4b06895cfce354364b30762b5acdd4f316
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-22 12:21:04 +0900

    created

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -1,8 +1,5 @@
 <template>
-    <NuxtLayout name="auth">
-        <template #header>
-            <LayoutAuthLoginHeader />
-        </template>
+    <NuxtLayout name="daisyui">
         <template #footer>
             <LayoutAuthLoginFooter
                 class="container"
@@ -10,22 +7,43 @@
         </template>
 
         <div class="container">
-            <h1 class="text-xl mb-16">
+            <h1 class="text-xl">
                 Login
             </h1>
-            <FormTextInput
-                v-for="(form, index) in forms" 
-                :key="index"
-                :model-value="form.value"
-                :label-top="form.labelTop"
-                :placeholder="form.placeholder"
-                :is-disabled="form.isDisabled"
-                :label-bottom="form.labelBottom"
-                :width="form.width"
-                :max-width="form.maxWidth"
-                @update:model-value="test($event, index)"
-            />
+            <hr>
 
+            <FormTag
+                :method="formTag.method"
+                :action="formTag.action"
+                :name="formTag.name"
+                :event="formTag.event"
+                class="needs-validation"
+                novalidate
+                @form-event="submitHandler($event)"
+            >
+                <FormTextInput
+                    v-for="(formTextInput, index) in formTextInputs" 
+                    :key="index"
+                    :model-value="formTextInput.value"
+                    :type="formTextInput.type"
+                    :name="formTextInput.name"
+                    :label-top="formTextInput.labelTop"
+                    :placeholder="formTextInput.placeholder"
+                    :is-required="formTextInput.isRequired"
+                    :is-disabled="formTextInput.isDisabled"
+                    :label-bottom="formTextInput.labelBottom"
+                    :width="formTextInput.width"
+                    :max-width="formTextInput.maxWidth"
+                    @update:model-value="testFormTextInput($event, index)"
+                />
+               
+                <input
+                    type="submit"
+                    class="btn btn-primary btn-wide"
+                    value="login"
+                />
+
+            </FormTag>
         </div>
     </NuxtLayout>
 </template>
@@ -35,41 +53,121 @@
         layout: false,
     });
 
+    // config
+    const config = useRuntimeConfig();
+
+    // FormTag
+    const formTag = reactive({
+        method: <string>'post',
+        action: <string>'/auth/login',
+        name: <string>'login',
+        event: <string>'login'
+    })
+    
     // FormTextInput
-    const forms = reactive([
+    const formTextInputs = reactive([
         {
-            value: <string>'',
-            labelTop: <string>'First Name',
+            value: <string>'edsoif@dfj.com',
+            type: <string>'email',
+            name: <string>'email',
+            labelTop: <string>'Email',
             placeholder: <string>'Type here',
+            isRequired: <boolean>true,
             isDisabled: <boolean>false,
             labelBottom: <string>'required',
             width: <string>'full',
             maxWidth: <string>'xs',
         },
         {
-            value: <string>'',
-            labelTop: <string>'Last Name',
+            value: <string>'password',
+            type: <string>'password',
+            name: <string>'password',
+            labelTop: <string>'Password',
             placeholder: <string>'Type here',
+            isRequired: <boolean>true,
             isDisabled: <boolean>false,
             labelBottom: <string>'required',
-            width: <string>'full',
-            maxWidth: <string>'xs',
-        },
-        {
-            value: <string>'',
-            labelTop: <string>'',
-            placeholder: <string>'Type here',
-            isDisabled: <boolean>true,
-            labelBottom: <string>'',
             width: <string>'full',
             maxWidth: <string>'xs',
         }
     ])
 
-    const test = (v: string, i: number) => {
-        // console.log(v)
-        // console.log(i)
-        forms[i].value = v
-        console.log(forms[i].value)
+    const testFormTextInput = (v: string, i: number) => {
+        formTextInputs[i].value = v
+        // console.log(formTextInputs[i].value)
+    }
+
+    // // FormSelect
+    const formSelect = reactive({
+        value: <string>'',
+        name: <string>'',
+        options: [
+            'test01',
+            'test02',
+            'test03'
+        ],
+        labelTop: <string>'Last Name',
+        placeholder: <string>'Type here',
+        isRequired: <boolean>true,
+        isDisabled: <boolean>false,
+        labelBottom: <string>'required',
+        width: <string>'60',
+        maxWidth: <string>'xs',
+    })
+    const testFormSelect = (v: string) => {
+        console.log(v)
+    }
+
+    // // FormCheckbox
+    const formCheckbox = reactive({
+        value: <boolean>false,
+        labelTop: <string>'Last Name',
+        isRequired: <boolean>true,
+        isDisabled: <boolean>false,
+        labelBottom: <string>'required',
+    })
+    const testFormCheckbox = (v: boolean) => {
+        console.log(v)
+    }
+
+    // FormButton
+    const formButton  = reactive({
+        value: <string>'login',
+        disabled: <boolean>false,
+    })
+    const testFormButton = (v: boolean) => {
+        formButton.disabled = v
+    }
+
+    const ableFormButton = () => {
+        formButton.disabled = false
+    }
+
+    const submitHandler = async (v: string) => {
+        const payload = {
+            email: <string>formTextInputs[0].value,
+            password: <string>formTextInputs[1].value
+        }
+
+        const requestOptions = {
+            method: "POST",
+            body: JSON.stringify(payload),
+        }
+
+        console.log(JSON.stringify(payload))
+
+        fetch(
+            `${config.APIBaseURL}/auth/login`,
+            requestOptions
+        )
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.error) {
+                console.log("Error:", data.message);
+            } else {
+                console.log(data)
+            }
+        })
+        
     }
 </script>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,90 +1,26 @@
 <template>
-    <div>
-        <NuxtLayout name="daisyui">
-            <template #header>
-                <LazyTheHeader v-if="show" />
-            </template>
-            <template #footer>
-                Some footer template content.
-            </template>
-            <div class="toast">
-                <div class="alert alert-info">
-                    <div>
-                        <span>New message arrived.</span>
-                    </div>
-                </div>
-            </div>
-            <div class="p-4">
-                <div class="tw-max-w-sm tw-rounded tw-overflow-hidden tw-shadow-lg tw-mb-4">
-                    <img
-                        class="tw-w-full"
-                        src="https://tailwindcss.com/img/card-top.jpg"
-                        alt="Sunset in the mountains"
-                    />
-                    <div class="tw-px-6 tw-py-4 tw-pb-2">
-                        <div class="tw-font-bold tw-text-xl tw-mb-2">
-                            The Coldest Sunset
-                        </div>
-                        <p class="tw-text-indigo-700 tw-text-base">
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus
-                            quia, nulla! Maiores et perferendis eaque, exercitationem praesentium
-                            nihil.
-                        </p>
-                    </div>
-                    <div class="tw-px-6 tw-py-4">
-                        <span class="tw-badge tw-mr-2">
-                            #photography
-                        </span>
-                        <span class="tw-badge tw-mr-2">
-                            #travel
-                        </span>
-                        <span class="tw-badge">
-                            #winter
-                        </span>
-                    </div>
-                </div>
-                <NuxtLink to="/about">
-                    About
-                </NuxtLink>
-            </div>
-            <button
-                v-if="!show"
-                @click="show = true"
-                class="tw-btn"
-            >
-                Show List
-            </button>
-            <div class="tw-w-48">
-                <div
-                class="tw-text-white tw-text-center tw-bg-clip-border tw-p-6 tw-bg-violet-600 tw-border-4 tw-border-violet-300 tw-border-dashed"
-                >
-                    Hello World
-                </div>
-            </div>
-        </NuxtLayout>
-    </div>
+    <NuxtLayout name="daisyui">
+        <template #header>
+            <LayoutDaisyuiIndexHeader />
+        </template>
+        <template #footer>
+            <LayoutDaisyuiIndexFooter />
+        </template>
+
+        <PageIndex
+            :container="page.container"
+            :padding-y="page.paddingY"
+        />
+
+    </NuxtLayout>
 </template>
 
-<script setup>
+<script lang="ts" setup>
     definePageMeta({
         layout: false,
     });
-    const show = ref(false)
+    const page = reactive({
+        container: <boolean>true,
+        paddingY: <string>'4'
+    })
 </script>
-
-<style lang="postcss" scoped>
-    /* .badge {
-        @apply
-            inline-block
-            bg-gray-200
-            rounded-full
-            px-3
-            py-1
-            text-sm
-            font-semibold
-            text-gray-700;
-        &:hover {
-            @apply bg-gray-300;
-        }
-    } */
-</style>
