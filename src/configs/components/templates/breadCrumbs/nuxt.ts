ace2bc3183d7a5cedc7b7023e8ac4d887c74ff96
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-27 01:01:39 +0900

    breadCrumbsのconfigを作成

---
+++ src/configs/components/templates/breadCrumbs/nuxt.ts
@@ -0,0 +1,11 @@
+export default ([
+  {
+    value: 'nuxt',
+    link: false,
+    path: '/nuxt',
+    text: {
+      en: 'Nuxt',
+      ja: 'Nuxt'
+    }
+  }
+]);
---
+++ src/configs/components/templates/breadCrumbs/nuxtDataFetching.ts
@@ -0,0 +1,20 @@
+export default ([
+  {
+    value: 'nuxt',
+    link: true,
+    path: '/nuxt',
+    text: {
+      en: 'Nuxt',
+      ja: 'Nuxt'
+    }
+  },
+  {
+    value: 'Data Fetching',
+    link: false,
+    path: '/nuxt/dataFetching',
+    text: {
+      en: 'Data Fetching',
+      ja: 'データフェッチング'
+    }
+  },
+]);
