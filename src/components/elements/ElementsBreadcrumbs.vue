aa8886192546f507390b1c4ce656f4777bcf18b3
Iwashita Hiroshi <hiroshi.iwashita.1991@gmail.com>
2022-12-27 01:01:08 +0900

    breadCrumbsコンポーネントをtemplateとelementに切り分け

---
+++ src/components/elements/ElementsBreadcrumbs.vue
@@ -0,0 +1,34 @@
+<template>
+  <component
+    :is="item.link
+      ? NuxtLink
+      : 'span'"
+    :to="item.link
+      ? item.path
+      : ''"
+    :class="item.link
+      ? 'link link-hover'
+      : ''"
+  >
+    {{ item.text.en }}
+  </component>
+</template>
+
+<script lang="ts" setup>
+  import { TBreadCrumbs } from "~~/types/components/templates/TBreadCrumbs";
+
+  // prop
+  interface Props {
+    item: TBreadCrumbs
+  }
+
+  const props = withDefaults(
+    defineProps<Props>(), {
+      item: () => <TBreadCrumbs>{},
+    }
+  );
+
+  // Dynamic components
+  const NuxtLink = resolveComponent('NuxtLink')
+
+</script>
---
+++ src/components/templates/TemplatesBreadcrumbs.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="breadcrumbs mb-8 text-sm">
+    <ul>
+      <li>
+        <NuxtLink
+          to="/"
+          class="link link-hover"
+        >
+          Home
+        </NuxtLink>
+      </li>
+      <template v-if="items.length"> 
+        <li
+          v-for="(item, key) in items"
+          :key="key"
+        >
+          <ElementsBreadcrumbs
+            :item="item"
+          />
+        </li>
+      </template>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { TBreadCrumbs } from "~~/types/components/templates/TBreadCrumbs";
+
+  // prop
+  interface Props {
+    items?: TBreadCrumbs[]
+    fontSize?: string
+  }
+
+  const props = withDefaults(
+    defineProps<Props>(), {
+      items: () => [],
+      fontSize: 'md'
+    }
+  );
+
+  // Dynamic components
+  const NuxtLink = resolveComponent('NuxtLink')
+
+  // classObject
+  const classObject = computed(() => {
+    return {
+      'text-xs' : props.fontSize === 'xs',
+      'text-sm' : props.fontSize === 'sm',
+      'text-md' : props.fontSize === 'md',
+      'text-lg' : props.fontSize === 'lg',
+      'text-xl' : props.fontSize === 'xl'
+    }
+  })
+
+</script>
